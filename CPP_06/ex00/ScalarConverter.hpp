fcc4aa42794e1030d906b309c454fcc73d01a814
Chaneze Mebarki <chamebar@f1br3s2.clusters.42paris.fr>
2025-12-16 17:53:24 +0100

    CPP 06 ex00 hpp

--- CPP_06/ex00/ScalarConverter.hpp
+++ CPP_06/ex00/ScalarConverter.hpp
@@ -6,7 +6,21 @@
 /*   By: chamebar <chamebar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 17:48:44 by chamebar          #+#    #+#             */
-/*   Updated: 2025/12/16 17:48:45 by chamebar         ###   ########.fr       */
+/*   Updated: 2025/12/16 17:53:08 by chamebar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+
+class ScalarConverter
+{
+	public :
+		static void convert(std::string& str);
+		
+		//Rajouter forme canonique ??
+};
+
+#endif
