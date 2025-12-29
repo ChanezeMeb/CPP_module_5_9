a75191db898c1830e6ff27286f1838896d918aa0
Chaneze <mebarki.chaneze96@gmail.com>
2025-12-30 00:01:14 +0100

    CPP08 creation fichiers exercices 02

--- CPP_08/ex02/main.cpp
+++ CPP_08/ex02/main.cpp
@@ -6,7 +6,41 @@
 /*   By: chamebar <chamebar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/29 23:46:19 by chamebar          #+#    #+#             */
-/*   Updated: 2025/12/29 23:46:20 by chamebar         ###   ########.fr       */
+/*   Updated: 2025/12/30 00:00:45 by chamebar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "MutanStack.hpp"
+
+int main()
+{
+    MutantStack<int> mstack;
+    
+    mstack.push(5);
+    mstack.push(17);
+
+    std::cout << mstack.top() << std::endl;
+    
+    mstack.pop();
+    
+    std::cout << mstack.size() << std::endl;
+    
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+    
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+    
+    ++it;
+    --it;
+    while (it != ite)
+    {
+        std::cout << *it << std::endl;
+        ++it;
+    }
+    std::stack<int> s(mstack);
+    return 0;
+}
