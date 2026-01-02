748de9561bbf437c5c84939f2f33a0c68ab8565a
Chaneze Mebarki <chamebar@f1r10s13.clusters.42paris.fr>
2026-01-02 17:58:15 +0100

    CPP09 ex00 arborescence

--- CPP_09/ex00/BitcoinExchange.hpp
+++ CPP_09/ex00/BitcoinExchange.hpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <map>
 
+//Creer une classe car la forme canonique est demande.
+
 std::map<std::string, double> load_csv(const std::string& filename);
 bool validate_date(const std::string date);
 bool validate_qty(int qty);
