a451db4c0ff0506183b88ef9f183322dd19ed330
Chaneze <mebarki.chaneze96@gmail.com>
2026-01-08 18:31:27 +0100

    CPP09 ex02 class

---
+++ CPP_09/ex02/PmergeMe.cpp
@@ -0,0 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chamebar <chamebar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/08 15:27:40 by chamebar          #+#    #+#             */
+/*   Updated: 2026/01/08 17:03:06 by chamebar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PmergeMe.hpp"
+
+//https://medium.com/@mohammad.ali.ibrahim.525/ford-johnson-algorithm-merge-insertion-4b024f0c3d42
---
+++ CPP_09/ex02/PmergeMe.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chamebar <chamebar@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/08 15:27:43 by chamebar          #+#    #+#             */
+/*   Updated: 2026/01/08 18:31:12 by chamebar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+class PmergeMe
+{
+    private :
+        
+    public :
+};
+
+#endif
+
+//Fonction a implementer dans la classe
+//L'algorithme de Ford-Johnson qui va se decouper en plusieurs fonctions
+
+//Etape 1 :
+//Fonction qui va creer les pairs et les trier ?
+
+//Etape 2 :
+//Fonction qui va separer en deux tableaux ? main et pend ?
+
+//Etape 3 : 
+//Utilisation de Jacobsthal ?
+
+//Etape 4 :
+//Insertion dans l'ordre ?
+
+//Affichage de deque et vector avant et apres le tri
+//displayBefore()
+//displayAfter()
+
+//Affichage du temps que met chaque container pour trier
+//timeVector()
+//timeDeque()
