b6147bc150f6aafc4e32fbc1e7a1f600ded3a6da
Chaneze <mebarki.chaneze96@gmail.com>
2026-01-12 17:31:34 +0100

    CPP09 ex02 classe en cours

--- CPP_09/ex02/PmergeMe.hpp
+++ CPP_09/ex02/PmergeMe.hpp
@@ -6,18 +6,35 @@
 /*   By: chamebar <chamebar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 15:27:43 by chamebar          #+#    #+#             */
-/*   Updated: 2026/01/08 18:31:12 by chamebar         ###   ########.fr       */
+/*   Updated: 2026/01/12 17:25:04 by chamebar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PMERGEME_HPP
 #define PMERGEME_HPP
 
+#include <vector>
+#include <deque>
+
 class PmergeMe
 {
     private :
-        
+        std::vector<int> _vector;
+        std::deque<int> _deque;
+        int _odd; //Element qui reste en cas de tableau impair
+        int _size; //taille du tableau
+        std::vector<std::pair<int, int> > makePairs(); //fonction pour faire des pairs
+        void mergeSort(std::vector<std::pair<int, int> > &pairs);
+        void merge(std::vector<std::pair<int, int> > &pairs); //merge pour left et right ??
+        //merge sort pour left et right ??
+        std::vector<int> jacobSthal(int n); //fonction qui calcule l'indice de jacobsthal
+        std::vector<int> insertionOrder(int n);
+        void parsing(char**argv); //fonction parsing
     public :
+        void displayBefore(); // mettre template dans ces fonctions ?
+        void displayAfter();
+        void timeVector();
+        void timeDeque();
 };
 
 #endif
