77d9d2a706308a8f4340f6d43b87459a0749b43d
Chaneze <mebarki.chaneze96@gmail.com>
2026-01-14 10:42:51 +0100

    CPP09 ex02 classe en cours

--- CPP_09/ex02/PmergeMe.cpp
+++ CPP_09/ex02/PmergeMe.cpp
@@ -6,10 +6,42 @@
 /*   By: chamebar <chamebar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 15:27:40 by chamebar          #+#    #+#             */
-/*   Updated: 2026/01/08 17:03:06 by chamebar         ###   ########.fr       */
+/*   Updated: 2026/01/14 10:42:19 by chamebar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
 
 //https://medium.com/@mohammad.ali.ibrahim.525/ford-johnson-algorithm-merge-insertion-4b024f0c3d42
+
+//Forme canonique
+PmergeMe::PmergeMe() : _odd(0) {}
+
+PmergeMe::PmergeMe(const PmergeMe &other) : _odd(other._odd),  _vector(other._vector), _deque(other._deque) {}
+
+PmergeMe &PmergeMe::operator=(const PmergeMe &other)
+{
+    if (this != &other)
+    {
+        this->_vector = other._vector;
+        this->_deque = other._deque;
+        this->_odd = other._odd;
+    }
+}
+
+PmergeMe::~PmergeMe() {}
+
+//Parsing
+void parsing(char**argv)
+{
+    for (int i = 1; argv[i]; i++)
+    {
+        std::string str = argv[i];
+        for (int j = 0; j < str.size(); j++)
+        {
+            
+        }
+    }
+    
+}
+
--- CPP_09/ex02/PmergeMe.hpp
+++ CPP_09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: chamebar <chamebar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/08 15:27:43 by chamebar          #+#    #+#             */
-/*   Updated: 2026/01/12 17:37:18 by chamebar         ###   ########.fr       */
+/*   Updated: 2026/01/14 10:38:06 by chamebar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 #include <vector>
 #include <deque>
+#include <iostream>
+#include <string>
 
 class PmergeMe
 {
@@ -22,83 +24,38 @@ class PmergeMe
         std::vector<int> _vector;
         std::deque<int> _deque;
         int _odd; //Element qui reste en cas de tableau impair
-        int _size; //taille du tableau
-        std::vector<std::pair<int, int> > makePairs(); //fonction pour faire des pairs
-        void mergeSort(std::vector<std::pair<int, int> > &pairs);
-        void separateChain(std::vector<std::pair<int, int> > &pair, std::vector<std::pair<int, int> > &mainChain, std::vector<std::pair<int, int> > &pending);
+
+        //Ford-Johnson VECTOR
+        std::vector<std::pair<int, int> > makePairsVec(); //fonction pour faire des pairs
+        void mergeSortVec(std::vector<std::pair<int, int> > &pairs);
+        void separateVec(std::vector<std::pair<int, int> > &pair, std::vector<int> &mainChain, std::vector<int> &pend);
+        void insertPendVec(std::vector<int> &mainChain, std::vector<int> &pend);
+        //Ford-Johnson DEQUE
+        std::deque<std::pair<int, int> > makePairsDeq(); //fonction pour faire des pairs
+        void mergeSortDeq(std::deque<std::pair<int, int> > &pairs);
+        void separateDeq(std::deque<std::pair<int, int> > &pair, std::deque<int> &mainChain, std::deque<int> &pend);
+        void insertPendDeq(std::deque<int> &mainChain, std::deque<int> &pend);
+        
         std::vector<int> jacobSthal(int n); //fonction qui calcule l'indice de jacobsthal
         std::vector<int> insertionOrder(int n);
         void parsing(char**argv); //fonction parsing
     public :
-        void displayBefore(); // mettre template dans ces fonctions ?
-        void displayAfter();
+        PmergeMe(char** argv);
+        void processVec();
+        void processDeq();
+        void displayBefore(); 
+        void displayAfterVec();
+        void displayAfterDeq();
         void timeVector();
         void timeDeque();
+
+        //Forme canonique
+        PmergeMe();
+        PmergeMe(const PmergeMe &other);
+        PmergeMe &operator=(const PmergeMe &other);
+        ~PmergeMe();
 };
-
-// class PmergeMe
-// {
-// private:
-//     std::vector<int> _vector;
-//     std::deque<int> _deque;
-//     int _odd;
-//     int _size;
-    
-//     // TEMPLATES pour éviter duplication
-//     template <typename Container>
-//     std::vector<std::pair<int, int>> makePairs(Container& cont);
-    
-//     template <typename T>
-//     void mergeSort(std::vector<std::pair<T, T>>& pairs);
-    
-//     template <typename T>
-//     void merge(std::vector<std::pair<T, T>>& pairs,
-//                std::vector<std::pair<T, T>>& left,
-//                std::vector<std::pair<T, T>>& right);
-    
-//     template <typename Container>
-//     void separateChains(std::vector<std::pair<int, int>>& pairs,
-//                         Container& main,
-//                         Container& pend);
-    
-//     std::vector<int> generateJacobsthal(int n);
-//     std::vector<int> generateInsertionOrder(int n);
-    
-//     template <typename Container>
-//     void insertPending(Container& main,
-//                        Container& pend,
-//                        std::vector<int>& order);
-    
-//     template <typename Container>
-//     void fordJohnsonRecursive(Container& container);
-
-// public:
-//     int parsing(int argc, char **argv);
-//     void process();
-//     void processDeq();
-// };
 
 #endif
 
-//Fonction a implementer dans la classe
-//L'algorithme de Ford-Johnson qui va se decouper en plusieurs fonctions
-
-//Etape 1 :
-//Fonction qui va creer les pairs et les trier ?
-
-//Etape 2 :
-//Fonction qui va separer en deux tableaux ? main et pend ?
-
-//Etape 3 : 
-//Utilisation de Jacobsthal ?
-
-//Etape 4 :
-//Insertion dans l'ordre ?
-
-//Affichage de deque et vector avant et apres le tri
-//displayBefore()
-//displayAfter()
-
-//Affichage du temps que met chaque container pour trier
-//timeVector()
-//timeDeque()
+//j'ai deja _odd pour l'element impair. Pourquoi avoir besoin de mergeVec alors qu'on a deja mergeSortVec ? Je peux appeler en recursif non ? pourrais tu  me reexpliquer la suite de Jacobsthal, insertPendVec et le binary sreach ? Pourquoi avoir besoin de argc ? Si dans le main je mets if argc < 2 error je n'en ai plus besoin par la suite.
